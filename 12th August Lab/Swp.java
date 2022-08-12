cd667b036fc102c0c2c5b1070ecacd9a89f13839
Deepraj Podder <deeprajpodder7@gmail.com>
2022-08-12 17:21:17 +0530

    Update Swp.java

--- 12th August Lab/Swp.java
+++ 12th August Lab/Swp.java
@@ -7,8 +7,8 @@ int a=10;
 int b=20;
 System.out.println("The first number is:" + a);
 System.out.println("The second number is:" + b);
-int temp;
-temp=a;
+int temp; //creating a temporary variable to store the data that is to be swapped
+temp=a;//swapping of variables begin
 a=b;
 b=temp;
 System.out.println("After swapping the numbers");
