126ef8175aba3dc08587c2904e4e4e0179b20c93
Deepraj Podder <deeprajpodder7@gmail.com>
2022-08-12 17:19:45 +0530

    Update Math.java

--- 12th August Lab/Math.java
+++ 12th August Lab/Math.java
@@ -8,11 +8,11 @@ System.out.print("The two numbers are:");
 int a = 125;
 int b = 24;
 System.out.println(" " + a + " " + b);
-int sum= a+b;
-int product= a*b;
-int division= a/b;
-int subtract= a-b;
-int remainder= a%b;
+int sum= a+b;//addition of the two numbers
+int product= a*b;//multiplication of the two numbers
+int division= a/b;//division of the two numbers
+int subtract= a-b;//subtraction of the two numbers
+int remainder= a%b;//reaminder of the two numbers after division
 System.out.println("The sum is :" + sum);
 System.out.println("The product is :" + product);
 System.out.println("The divided result is :" + division);
